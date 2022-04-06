9b661f119778cd1e1701fd56adebb410bc278ad1
juliojimenez98 <juliojimenez1998@gmail.com>
2022-04-05 22:15:26 -0400

    perfil, navbar, chat

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { BlogComponent } from './blog/blog.component';
+import { ChatComponent } from './chat/chat.component';
 import { ProfileComponent } from './profile/profile.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'blog', component: BlogComponent },
   { path: 'perfil', component: ProfileComponent },
+  { path: 'chat', component: ChatComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,19 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './auth/login/login.component';
 import { BlogComponent } from './blog/blog.component';
 import { ProfileComponent } from './profile/profile.component';
+import { ChatComponent } from './chat/chat.component';
+import { NavbarComponent } from './share/navbar/navbar.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     BlogComponent,
-    ProfileComponent
+    ProfileComponent,
+    ChatComponent,
+    NavbarComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
